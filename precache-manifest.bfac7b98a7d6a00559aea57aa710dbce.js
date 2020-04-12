b195e7546f4a1ed27f9e0d34bb16ebd610b79300
Raja Mettu <rajasekhar.mettu@gmail.com>
2020-04-12 01:48:45 -0400

    Updates

---
+++ precache-manifest.bfac7b98a7d6a00559aea57aa710dbce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a40cfe87706f2e7fe1748b5f1f0a83",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "91b5a73862b025d7a46d",
+    "url": "/monster-rolodex/static/css/main.9ecdacbe.chunk.css"
+  },
+  {
+    "revision": "6ce0f7ae1c829bfb1e06",
+    "url": "/monster-rolodex/static/js/2.156d7307.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.156d7307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b5a73862b025d7a46d",
+    "url": "/monster-rolodex/static/js/main.236e0122.chunk.js"
+  },
+  {
+    "revision": "0c8a7fc9e6e1eed33bc3",
+    "url": "/monster-rolodex/static/js/runtime-main.2f5da205.js"
+  }
+]);
